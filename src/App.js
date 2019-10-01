76ac37c80d8e5ca5b703f5660015f9acb31e2879
KennethDyerHill <52476371+KennethDyerHill@users.noreply.github.com>
2019-10-01 12:23:16 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,68 @@
 import React from 'react';
+import TodoForm from './components/TodoComponents/TodoForm';
+import TodoList from './components/TodoComponents/TodoList';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    }
+    
+  }
+
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  
+  addTodo = event => {
+    event.preventDefault();
+    const todos = this.state.todos.slice();
+    todos.push({ task: this.state.todo, completed: false, id: Date.now() });
+    this.setState({ todos, todo: '' });
+  };
+
+  changeTodo = event => this.setState({ [event.target.name]: event.target.value });
+
+  toggleTodoComplete = id => {
+    let todos = this.state.todos.slice();
+    todos = todos.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        return todo;
+      } else {
+        return todo;
+      }
+    });
+    this.setState({ todos });
+  };
+
+  clearCompletedTodos = event => {
+    event.preventDefault();
+    let todos = this.state.todos.slice();
+    todos = todos.filter(todo => todo.completed === false);
+    this.setState({ todos });
+  };
+  
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <TodoList 
+          handleToggleComplete = {this.toggleTodoComplete}
+          todos={this.state.todos}
+         />
+        <TodoForm
+          value = {this.state.todo}
+          handleTodoChange = {this.changeTodo}
+          handleAddTodo = {this.addTodo}
+          handleClearTodos = {this.clearCompletedTodos}
+         />
       </div>
     );
   }
 }
 
 export default App;
+// this.state.todos.map((item) => {
+//   return item.task;
+// })
